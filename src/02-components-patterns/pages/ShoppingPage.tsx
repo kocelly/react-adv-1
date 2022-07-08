83ffd15bda7a3a78ed9c3885ffe6246e1d93e7d4
kocelly <kocelly@readymind.ms>
2022-07-07 23:57:09 -0500

    basic traditional component

---
+++ src/02-components-patterns/pages/ShoppingPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { ProductCard } from '../components/ProductCard'
+
+
+export const ShoppingPage = () => {
+  return (
+    <div>
+      <h1>ShoppingPage</h1>
+      <hr />
+
+      <ProductCard />
+    </div>
+  )
+}
